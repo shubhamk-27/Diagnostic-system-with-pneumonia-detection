804c361b11ca05696c075039d23cc2c498d49e2c
Shubham Koli <shubhamkoli1927@gmail.com>
2023-02-06 17:53:42 +0530

    changed backend to localhost

--- doctor-note/util/api.js
+++ doctor-note/util/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://doctor-note-api.herokuapp.com/" });
+const api = axios.create({ baseURL: "http://localhost:3005/" });
 
 api.interceptors.request.use(
   function (config) {
